0b9ae7ba110adaa7931700d581acb732b7d01d14
PranjalUpadhyay1 <pranjalupadhyay25@gmail.com>
2025-12-09 23:39:59 +0530

    dist debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AI-Component-Generator/', // Set this to your GitHub repository name
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
